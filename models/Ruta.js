f10dd4ddc2d910f9e9ee1568158218cfd010ffe2
jlgaibor <josejarrin3@gmail.com>
2025-08-25 14:24:30 -0500

    Update Ruta.js

--- models/Ruta.js
+++ models/Ruta.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-
 const rutaSchema = new mongoose.Schema({
-  supervisorId: String,
-  choferId: String,
-  puntos: [String], // nombres de áreas o coordenadas
+  supervisorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  choferId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  puntos: [{ lat: Number, lng: Number, estado: String }],
   fecha: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model("Ruta", rutaSchema);
