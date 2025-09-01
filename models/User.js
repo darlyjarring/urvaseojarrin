c434985a962dd5466730328c15595295a47843ee
jlgaibor <josejarrin3@gmail.com>
2025-09-01 13:01:02 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },  // puede ser el correo
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["chofer", "supervisor", "admin"], required: true }
+ role: { type: String, enum: ["chofer", "supervisor", "admin"], required: true }
+
+  // 👇 nuevos campos
+  cedula: { type: String, required: true },
+  nombres: { type: String, required: true },
+  apellidos: { type: String, required: true }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
