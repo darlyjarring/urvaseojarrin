0100a692361da9760da5289fb9c8e2cff0df5b0e
jlgaibor <josejarrin3@gmail.com>
2025-09-04 10:43:02 -0500

    Update Placa.js

--- models/Placa.js
+++ models/Placa.js
@@ -1,18 +1,1 @@
-// models/Placa.js
-const mongoose = require("mongoose");
 
-const PlacaSchema = new mongoose.Schema({
-  placa: { 
-    type: String, 
-    required: true, 
-    unique: true, 
-    trim: true 
-  },
-  estado: { 
-    type: String, 
-    enum: ["activo", "inactivo"], 
-    default: "activo" 
-  }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Placa", PlacaSchema);
